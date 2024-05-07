b9d2f51cb03e0d085a3488f5a3a98c04d5381969
Samrat Pant <Samrat Pant>
2024-05-07 23:56:42 +1000

    feature optimise tsv parser utility

--- test/utils/custom_tsv_parser.py
+++ test/utils/custom_tsv_parser.py
@@ -4,32 +4,27 @@ import csv
 class ParseTSVFile:
     def __init__(self, file_path):
         self.file_path = file_path
+        self.data = self._read_file()
+
+    def _read_file(self):
         with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            self.reader = csv.DictReader(file, delimiter="\t")
+            reader = csv.DictReader(file, delimiter="\t")
+            return list(reader)
 
     def get_columns(self):
-        return self.reader.fieldnames
+        return self.data[0].keys()
 
     def get_row_count(self):
-        return len(list(self.reader))
+        return len(self.data)
 
     def get_column_count(self):
-        return len(self.reader.fieldnames)
+        return len(self.data[0].keys())
 
     def get_rows(self):
-        data = []
-        for row in self.reader:
-            data.append(row)
-        return data
+        return self.data
 
     def get_column_data(self, column_name):
-        data = []
-        for row in self.reader:
-            data.append(row[column_name])
-        return data
+        return [row[column_name] for row in self.data]
 
     def get_row_data(self, row_number):
-        data = []
-        for row in self.reader:
-            data.append(row)
-        return data[row_number]
+        return self.data[row_number]
