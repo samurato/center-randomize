d2bc4136c2a2b07a95063cb7911666068b6dc08c
Samrat Pant <Samrat Pant>
2024-05-07 23:04:39 +1000

    feature optimise tsv parser utility

--- test/utils/custom_tsv_parser.py
+++ test/utils/custom_tsv_parser.py
@@ -4,47 +4,32 @@ import csv
 class ParseTSVFile:
     def __init__(self, file_path):
         self.file_path = file_path
-
-    def read_file(self):
         with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            return reader
+            self.reader = csv.DictReader(file, delimiter="\t")
 
     def get_columns(self):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            return reader.fieldnames
+        return self.reader.fieldnames
 
     def get_row_count(self):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            return len(list(reader))
+        return len(list(self.reader))
 
     def get_column_count(self):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            return len(reader.fieldnames)
+        return len(self.reader.fieldnames)
 
     def get_rows(self):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            data = []
-            for row in reader:
-                data.append(row)
-            return data
+        data = []
+        for row in self.reader:
+            data.append(row)
+        return data
 
     def get_column_data(self, column_name):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            data = []
-            for row in reader:
-                data.append(row[column_name])
-            return data
+        data = []
+        for row in self.reader:
+            data.append(row[column_name])
+        return data
 
     def get_row_data(self, row_number):
-        with open(self.file_path, "r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter="\t")
-            data = []
-            for row in reader:
-                data.append(row)
-            return data[row_number]
+        data = []
+        for row in self.reader:
+            data.append(row)
+        return data[row_number]
